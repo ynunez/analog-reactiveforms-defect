e2319c3af2fed08399c1096491530a07e09b8b5f
Yoel Nunez <y.nunez@floms.com>
2024-04-27 02:38:14 -0400

    Initial commit

---
+++ src/app/pages/index.page.ts
@@ -0,0 +1,64 @@
+import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [ReactiveFormsModule, FormsModule, JsonPipe],
+  template: `
+    <div>
+      <a href="https://analogjs.org/" target="_blank">
+        <img alt="Analog Logo" class="logo analog" src="/analog.svg" />
+      </a>
+    </div>
+
+    <div class="max-w-sm mx-auto flex flex-col text-justify">
+      <form [formGroup]="profileForm">
+        <div class="mb-5">
+          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+          <input type="email" id="email" formControlName="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="your@email.com" required />
+        </div>
+        <div class="mb-5">
+          <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+          <input type="password" id="password" formControlName="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="password" required />
+        </div>
+        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+      </form>
+      <pre class="mt-10">{{ profileForm.value | json }}</pre>
+    </div>
+  `,
+  styles: [
+    `
+      .logo {
+        will-change: filter;
+      }
+      .logo:hover {
+        filter: drop-shadow(0 0 2em #646cffaa);
+      }
+      .read-the-docs {
+        color: #888;
+      }
+    `,
+  ],
+})
+export default class HomeComponent {
+  count = 0;
+
+  form = inject(FormBuilder);
+
+  profileForm = this.form.group({
+    email: [''],
+    password: [''],
+  });
+
+  submit() {
+    alert('submitted');
+    console.log(this.profileForm.value);
+  }
+}
